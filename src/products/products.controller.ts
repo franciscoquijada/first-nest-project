73685f26cb78308bfd6ad2c41df67cc7c51a06bf
fquijada <fquijada@centribal.com>
2021-11-02 13:04:25 -0300

    Completada prueba de cruds

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,15 +1,38 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
     @Get()
-    getAll(){
+    getAll() {
         return 'Este es el listado de productos';
     }
 
     @Get(':id')
-    find(@Param() params){
+    find(@Param() params) {
         return `El producto recibido tiene este id: ${params.id}`
+    }
+
+    @Post()
+    create(
+        @Body('name') name: string,
+        @Body('description') description: string
+    ){
+        return `Estas creando un producto con nombre ${name} y descripcion ${description}`
+    }
+
+    @Put(':id')
+    update(
+        @Param('id') id:number
+        @Body('name') name: string,
+        @Body('description') description: string
+    ){
+        return `Estas actualizando el producto ${id} con nombre ${name} y descripcion ${description}`
+    }
+
+
+    @Delete(':id')
+    delete(@Param('id') id: number) {
+        return JSON.stringify(`El producto con el id  ${id} fue eliminado`)
     }
 
 
